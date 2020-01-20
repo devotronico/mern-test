8b4a1e6c707d8ce4e5c7620398fed40f342a33b2
Daniele <dmanzi83@gmail.com>
2020-01-20 21:04:28 +0100

    commit 5

--- server.js
+++ server.js
@@ -12,9 +12,13 @@ mongoose.connect(
   }
 );
 
-app.get('/', (req, res) => {
-  res.send("Ciao Mondo");
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
