2579e55756e6e0067d7fe499f3801e378eecb742
Daniele <dmanzi83@gmail.com>
2020-01-20 21:00:40 +0100

    commit 4

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.get('/', (req, res) => {
   res.send("Ciao Mondo");
